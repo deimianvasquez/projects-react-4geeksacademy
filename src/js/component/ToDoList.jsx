8d7f7aa69ba4584cbb3782abf5ebc11cc06deeb1
Deimian Vasquez <deimianvasquez@gmail.com>
2020-08-18 16:38:55 -0400

    To Do List finalizado

--- src/js/component/ToDoList.jsx
+++ src/js/component/ToDoList.jsx
@@ -3,12 +3,28 @@ import React, { useState } from "react";
 const ToDoList = () => {
 	const [todos, setTodos] = useState([]);
 	const [task, setTask] = useState("");
+	const [error, setError] = useState(false);
 
 	const addTask = e => {
 		if (e.keyCode == 13) {
-			setTodos([...todos, { label: task, done: false }]);
-			setTask("");
+			if (task.trim() != "") {
+				setTodos([...todos, { label: task, done: false }]);
+				setTask("");
+				setError(false);
+				console.log("Datos llenos", error);
+			} else {
+				setError(true);
+				console.log("debe llenar todo los datos", error);
+			}
 		}
+	};
+
+	const deleteTask = index => {
+		setTodos(
+			todos.filter((task, i) => {
+				return i != index;
+			})
+		);
 	};
 
 	return (
@@ -16,6 +32,12 @@ const ToDoList = () => {
 			<div className="row justify-content-center">
 				<div className="col-md-6 ">
 					<h2 className="text-center">Todos</h2>
+					{error ? (
+						<div className="alert alert-danger text-center">
+							El campo de tarea no debe estar vacio, debe colocal
+							una tarea validad
+						</div>
+					) : null}
 					<input
 						type="text"
 						className="form-control my-2"
@@ -29,14 +51,22 @@ const ToDoList = () => {
 							return (
 								<li
 									key={index}
-									onClick={() => alert("debo eliminar")}>
+									onClick={() => deleteTask(index)}>
 									{task.label}
 									<div>x</div>
 								</li>
 							);
 						})}
 					</ul>
-					<label>tareas por hacer {todos.length}</label>
+					{todos.length < 1 ? (
+						<label>
+							Ya, terminaste las tareas CONGRATULATIONS!!!!
+						</label>
+					) : todos.length == 1 ? (
+						<label> Esta es la ultima tarea, solo falta una</label>
+					) : todos.length > 1 ? (
+						<label>Te faltan {todos.length} tareas</label>
+					) : null}
 				</div>
 			</div>
 		</div>
